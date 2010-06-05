6923ec0c5891cc961ba3427e86ba847dcdfa0945
Mike Petersen <mike@odania-it.de>
2010-06-05 13:20:17 +0200

    Display of adviews reworked

--- app/controllers/ad_views_controller.rb
+++ app/controllers/ad_views_controller.rb
@@ -1,11 +1,16 @@
 class AdViewsController < ApplicationController
+	unloadable
 	before_filter :check_admin_allowed
 	layout 'ads'
 
 	# GET /ad_views
 	# GET /ad_views.xml
 	def index
-		@ad_views = AdView.all
+		@ad_views = AdView.all(
+				:select => "COUNT(*) AS views, ad_id, advertiser_id, user_type, location, page, format",
+				:group => "ad_id, advertiser_id, user_type, location, page, format",
+				:order => "views DESC"
+			)
 
 		respond_to do |format|
 			format.html # index.html.erb
