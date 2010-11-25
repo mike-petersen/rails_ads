4d3ae906fa0d0610de9a1c567552de63da51e5c7
Mike Petersen <mike@odania-it.de>
2010-11-26 00:17:38 +0100

    Routes for Rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
-ActionController::Routing::Routes.draw do |map|
-	map.resources :ad_views
-	map.resources :ads
-	map.resources :advertisers
+Rails.application.routes.draw do
+	resources :ad_views
+	resources :ads
+	resources :advertisers
 end
